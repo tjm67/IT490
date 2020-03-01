270327f2277c1431066f63334cadd839c208ee89
tjm67 <46944099+tjm67@users.noreply.github.com>
2020-02-29 21:14:33 -0500

    Add files via upload

--- backend/Connect/connect.py
+++ backend/Connect/connect.py
@@ -1,10 +1,14 @@
 import psycopg2, time
 time.sleep(5)
+
 conn = None
 try:
 	conn = psycopg2.connect(host="it490_postgres_1",database="mydb",user="root",password="root")
 	cur = conn.cursor()
-	cur.execute("SELECT * from users")
+	cur.execute("SELECT * from users;")
+	print("Number of users: ", cur.rowcount)
+	cur.execute("INSERT INTO users VALUES ('anthony joshua', DEFAULT, 'pass');")
+	cur.execute("SELECT * from users;")
 	print("Number of users: ", cur.rowcount)
 	cur.close()
 except (Exception, psycopg2.DatabaseError) as error:
