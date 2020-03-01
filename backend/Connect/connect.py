c16af1d1957aeaf814096f492d3e001b3fdac46a
tjm67 <46944099+tjm67@users.noreply.github.com>
2020-02-29 20:57:14 -0500

    Add files via upload

---
+++ backend/Connect/connect.py
@@ -0,0 +1,13 @@
+import psycopg2
+conn = None
+try:
+	conn = psycopg2.connect(host="it490_postgres_1",database="mydb",user="root",password="root")
+	cur = conn.cursor()
+	cur.execute("SELECT * from users")
+	print("Number of users: ", cur.rowcount)
+	cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+finally:
+	if conn is not None:
+		conn.close()
