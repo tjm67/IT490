9836d0d86401fcd01b7f0378c761f0f26650cc83
jsb55 <48959322+jsb55@users.noreply.github.com>
2020-03-02 10:53:47 -0500

    Add files via upload

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/home/')
+@app.route('/')
 def Home():
     return render_template('index.html')
 
-@app.route('/home/login/')
+@app.route('/login/')
 def Login():
     return render_template('login.html')
 	
-@app.route('/home/register/')
+@app.route('/register/')
 def Register():
 	return render_template('register.html')
 	
