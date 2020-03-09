af6f6e7a1a7310922be8710be0fe0737d958b733
kg338 <50340944+kg338@users.noreply.github.com>
2020-03-09 09:48:06 -0400

    Update connect.py

--- backend/Connect/connect.py
+++ backend/Connect/connect.py
@@ -18,26 +18,3 @@ channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
-
-time.sleep(25)
-
-conn = None
-try:
-	conn = psycopg2.connect(host="it490_postgres_1",database="mydb",user="root",password="root")
-	cur = conn.cursor()
-	
-	cur.execute("CREATE TABLE IF NOT EXISTS users (name varchar(50) NOT NULL, id SERIAL, password varchar(30) NOT NULL);")
-	cur.execute("SELECT * from users;")
-	print("Number of users: ", cur.rowcount)
-	cur.execute("INSERT INTO users VALUES ('anthony', DEFAULT, 'pass');")
-	conn.commit()
-	cur.execute("SELECT * from users;")
-	print("Number of users: ", cur.rowcount)
-	print(cur.fetchall())
-	cur.close()
-	
-except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-finally:
-	if conn is not None:
-		conn.close()
